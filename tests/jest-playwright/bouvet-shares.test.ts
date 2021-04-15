010f48713dd58f370ace957578eca7e15cdae8a9
Oscar Carlstrom <oscar.carlstrom@SN-PF211DPN.localdomain>
2021-04-15 09:43:10 +0200

    Added tests for 'Bouvet deler' page

---
+++ tests/jest-playwright/bouvet-shares.test.ts
@@ -0,0 +1,19 @@
+jest.setTimeout(180 * 1000);
+
+beforeAll(async () => {
+  await page.goto('https://bouvet.no/bouvet-deler');
+});
+
+test('Should have correct heading', async () => {
+  await expect(page).toHaveSelector('//h1[text()="Bouvet deler"]');
+});
+
+test('Should be possible to filter', async () => {
+  //Her burde vi egentlig kunne bruke page.waitForNavigation()
+  //Men det er to requester som fyres av mot https://bouvet.no/bouvet-deler?design=true her
+  //Som gjør at firefox ikke greier å resolve den første som en navigation
+  //Vi bruker også Promise.all for å unngå race conditions
+  await Promise.all([page.click('label[for="filter-design"]'), page.waitForLoadState()]);
+  expect(page.url().includes('?design=true')).toBe(true);
+  await expect(page).toHaveSelector('"Bærekraftig programmering"');
+});
